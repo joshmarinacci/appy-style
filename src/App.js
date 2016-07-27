ed0da3ada48c018196aff1284259ed2b435d49ee
Josh Marinacci <joshua@marinacci.org>
2016-07-27 10:02:39 -0700

    added dialog

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import React, { Component } from 'react';
 import './layout.css';
+import TunesDemo from "./TunesDemo";
+import DialogDemo from "./DialogDemo";
 
-class TunesDemo extends Component {
-    render() {
-        return (
-            <div className='fill vbox debug'>
-                <div className='hbox'>
-                    <button>play</button>
-                    <label>song name</label>
-                    <span className='grow'></span>
-                    <input type='search' placeholder='search for songs'/>
-                </div>
-
-
-                <div className='hbox debug grow'>
-                    <div className='vbox'>
-                        <h3>sources</h3>
-                        <div class='scroll grow'>
-                            <ul>
-                                <li>All songs</li>
-                                <li>Artists</li>
-                                <li>Albums</li>
-                                <li>Genres</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className='grow scroll'>
-                        <table>
-                            <tbody>
-                            .... table of songs
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div className='hbox'>
-                    <label>Songs in library: 23,000</label>
-                </div>
-
-            </div>
-        );
-    }
-}
 class App extends Component {
   render() {
+      //              <TunesDemo/>
       return <div className="vbox fill">
           <h1>demos for you to watchs</h1>
           <div className="hbox"><button>Tunes</button></div>
           <div className="grow" id="demo-pane">
-              <TunesDemo/>
+              <DialogDemo/>
           </div>
       </div>
   }
